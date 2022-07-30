8e73722a0133180c64b8b1bcb9d2457d35fc982f
Mohamed Khaled Mohamed <mohamed.elbassiony00@eng-st.cu.edu.eg>
2022-07-30 16:31:31 +0200

    optimised bilateral

---
+++ ffmpegchroma.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+make -j 10
+x=1
+while [ $x -le 2 ]
+do
+    rm overlay_test.mp4
+    ./ffmpeg -v verbose \
+    -hwaccel cuda -hwaccel_output_format cuda -i input_green.mp4  \
+    -hwaccel cuda -hwaccel_output_format cuda -i sea.mp4 \
+    -init_hw_device cuda \
+    -filter_complex \
+    " \
+    [0:v]scale_cuda=format=yuv420p,chromakey_cuda=0x25302D:0.1:0.12:1[overlay_video];
+    [1:v]scale_cuda=format=yuv420p[base];
+    [base][overlay_video]overlay_cuda" \
+    -an -sn -c:v h264_nvenc -cq 20 overlay_test.mp4
+    x=$(( $x + 1 ))
+done
+mpv overlay_test.mp4
+rm overlay_test.mp4
+#scale_cuda=format=yuv420p,dummy_cuda
