8e004a90499586979bc8950c17b07754241df225
Mohamed Khaled Mohamed <mohamed.elbassiony00@eng-st.cu.edu.eg>
2022-08-15 17:31:17 +0200

    not important

--- run_bilateralnv12.sh
+++ run_bilateralnv12.sh
@@ -7,8 +7,8 @@ rm out.mp4
 -init_hw_device cuda \
 -filter_complex \
 " \
-[0:v]scale_cuda=format=yuv420p[scaled_video];
-[scaled_video]bilateral_cuda=window_size=9:sigmaS=3.0:sigmaR=50.0" \
+[0:v]scale_cuda=format=nv12[output_wanted];
+[output_wanted]bilateral_cuda=window_size=9:sigmaS=3.0:sigmaR=50.0" \
 -an -sn -c:v h264_nvenc -cq 20 out.mp4
 ffmpeg -i out.mp4 -vf "select=eq(n\,0)" -q:v 3 output_image.jpg
 mpv out.mp4
